3ccb76d811690724a61101dbe498f7a285d38e86
filipeportes <s3nh4@g1thub>
2018-01-26 14:19:45 +0100

    loading offers

--- server/main.js
+++ server/main.js
@@ -1,5 +1,29 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
+import {HTTP} from 'meteor/http';
+
+import {Offers} from '../imports/api/offers.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+    let offersCount = Offers.find().count();
+    if(offersCount === 0) {
+        HTTP.call("GET", "https://bonding.testraisin.com/rcba/v1/offer/DEU?locale=en", {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": "Bearer bonding1",
+            }
+        },
+        function (error, response) {
+            if(error) {
+                console.error(error);
+            } else {
+                _.each(response.data, function (offer) {
+                    Offers.insert(offer);
+                });
+
+                console.log("Offers loaded: " + Offers.find().count());
+            }
+        });
+    }
 });
