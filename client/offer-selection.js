437cbff8bd4e6ebed8ae0f0cb0a7a4ccbe517ccc
Felipe Costa <felipe.costa@raisin.com>
2018-01-26 18:03:37 +0100

    no message

--- client/offer-selection.js
+++ client/offer-selection.js
@@ -96,7 +96,9 @@ Template.offerSelection.onRendered(function() {
         datasetFill: true,
 
         //String - A legend template
-        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+        legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+
+        maintainAspectRatio: false
 
     };
 
