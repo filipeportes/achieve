57eb351a276eadf4e570df6443fa40f109e5a5ad
filipeportes <s3nh4@g1thub>
2018-01-26 13:10:55 +0100

    user registration and login

--- imports/startups/routes.js
+++ imports/startups/routes.js
@@ -1,3 +1,24 @@
+import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+AccountsTemplates.configure({
+    defaultLayout: 'Layout',
+    defaultContentRegion: 'main'
+});
+
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp');
+
+FlowRouter.route('/logout', {
+    name: 'logout',
+    action() {
+        Meteor.logout();
+        FlowRouter.go("/");
+    }
+});
+
 FlowRouter.route('/', {
     name: 'root',
     action() {
--- server/main.js
+++ server/main.js
@@ -1,5 +1,4 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/startups/routes.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
