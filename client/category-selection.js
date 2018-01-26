9444817aaa5241704ccaf76dbd78d1923f1e1fa2
Felipe Costa <felipe.costa@raisin.com>
2018-01-26 14:23:57 +0100

    Changes

--- client/category-selection.js
+++ client/category-selection.js
@@ -2,18 +2,7 @@ import { Categories } from '../imports/api/categories.js';
 
 Template.categorySelection.helpers({
     categories() {
-    	// return Categories.find({});
-    	return [
-	    	{
-	    		title: "Travel"
-	    	},
-	    	{
-	    		title: "Car"
-	    	},
-	    	{
-	    		title: "Savings"
-	    	}
-    	];
+    	return Categories.find({});
     }
 });
 
